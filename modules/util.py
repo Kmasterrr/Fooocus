642867366f29c2635bde1375379ab84ba745ecc3
George Karsas <george939@hotmail.com>
2024-01-20 00:21:58 +0200

    fix: utils

--- modules/util.py
+++ modules/util.py
@@ -1,36 +1,28 @@
 import numpy as np
 import datetime
 import random
+import math
 import os
+import cv2
 
 from PIL import Image
-
-
-def image_is_generated_in_current_ui(image, ui_width, ui_height):
-    H, W, C = image.shape
-    
-    if H < ui_height:
-        return False
-
-    if W < ui_width:
-        return False
-
-    # k1 = float(H) / float(W)
-    # k2 = float(ui_height) / float(ui_width)
-    # d = abs(k1 - k2)
-    #
-    # if d > 0.01:
-    #     return False
-
-    return True
 
 
 LANCZOS = (Image.Resampling.LANCZOS if hasattr(Image, 'Resampling') else Image.LANCZOS)
 
 
+def erode_or_dilate(x, k):
+    k = int(k)
+    if k > 0:
+        return cv2.dilate(x, kernel=np.ones(shape=(3, 3), dtype=np.uint8), iterations=k)
+    if k < 0:
+        return cv2.erode(x, kernel=np.ones(shape=(3, 3), dtype=np.uint8), iterations=-k)
+    return x
+
+
 def resample_image(im, width, height):
     im = Image.fromarray(im)
-    im = im.resize((width, height), resample=LANCZOS)
+    im = im.resize((int(width), int(height)), resample=LANCZOS)
     return np.array(im)
 
 
@@ -92,6 +84,35 @@ def resize_image(im, width, height, resize_mode=1):
     return np.array(res)
 
 
+def get_shape_ceil(h, w):
+    return math.ceil(((h * w) ** 0.5) / 64.0) * 64.0
+
+
+def get_image_shape_ceil(im):
+    H, W = im.shape[:2]
+    return get_shape_ceil(H, W)
+
+
+def set_image_shape_ceil(im, shape_ceil):
+    shape_ceil = float(shape_ceil)
+
+    H_origin, W_origin, _ = im.shape
+    H, W = H_origin, W_origin
+    
+    for _ in range(256):
+        current_shape_ceil = get_shape_ceil(H, W)
+        if abs(current_shape_ceil - shape_ceil) < 0.1:
+            break
+        k = shape_ceil / current_shape_ceil
+        H = int(round(float(H) * k / 64.0) * 64)
+        W = int(round(float(W) * k / 64.0) * 64)
+
+    if H == H_origin and W == W_origin:
+        return im
+
+    return resample_image(im, width=W, height=H)
+
+
 def HWC3(x):
     assert x.dtype == np.uint8
     if x.ndim == 2:
@@ -135,3 +156,22 @@ def generate_temp_filename(folder='./outputs/', extension='png'):
     filename = f"{time_string}_{random_number}.{extension}"
     result = os.path.join(folder, date_string, filename)
     return date_string, os.path.abspath(os.path.realpath(result)), filename
+
+
+def get_files_from_folder(folder_path, exensions=None, name_filter=None):
+    if not os.path.isdir(folder_path):
+        raise ValueError("Folder path is not a valid directory.")
+
+    filenames = []
+
+    for root, dirs, files in os.walk(folder_path):
+        relative_path = os.path.relpath(root, folder_path)
+        if relative_path == ".":
+            relative_path = ""
+        for filename in files:
+            _, file_extension = os.path.splitext(filename)
+            if (exensions == None or file_extension.lower() in exensions) and (name_filter == None or name_filter in _):
+                path = os.path.join(relative_path, filename)
+                filenames.append(path)
+
+    return sorted(filenames, key=lambda x: -1 if os.sep in x else 1)
